a6c5a9b8838f4f17b9c100e1f8b6e8b5e3608b58
kar <kkarkalos@yahoo.com>
2017-05-06 19:31:30 +0200

    add text

--- javatest/test1/src/main/java/com/testarmaven/spring/test/App.java
+++ javatest/test1/src/main/java/com/testarmaven/spring/test/App.java
@@ -6,6 +6,8 @@ public class App {
     public static void main(String[] args) {
         OcaQuestion ocaQuestion = new OcaQuestion();
 
+        System.out.println("test");
+
         ocaQuestion.testShower();
 
 
